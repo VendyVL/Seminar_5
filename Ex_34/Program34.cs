924ee47f21d609335ec60c4dca3cb68277b02035
«VendyVL» <vedmansk@yandex.ru>
2022-05-27 10:15:24 +0300

    задача 34

--- Ex_34/Program34.cs
+++ Ex_34/Program34.cs
@@ -1,3 +1,33 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
-Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Ведите размер массива");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[num];// В условии это не требуется, но и постоянная длинна не задана
+
+int[] PrintArray(int[] array) // из предыдущей задачи
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+        Console.Write(array[i] + " ");
+    }
+    return array;
+}
+
+PrintArray(arr);
+Console.WriteLine();
+int count=0;
+
+
+for (int i = 0; i < arr.Length;i++)
+{
+    if ((arr[i] % 2) == 0)
+    {
+        count++;
+    }
+
+}
+Console.WriteLine($"Количество чётных чисел в массиве = {count}");
